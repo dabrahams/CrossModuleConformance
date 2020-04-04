8300fb647c8d5c587026a81b6828fd7e9e7a4a19
Dave Abrahams <dabrahams@google.com>
2020-04-04 14:19:49 -0700

    Tweaks + README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Top
+# Swift Cross-Module Conformance Demo
 
-A description of this package.
+This little example demonstrates how Swift deals with conflicting conformances
+across modules.  There appears to be some dependence on import order, among
+other things.  It's easy enough to create inconsistent semantics; just comment
+out "import A" from "AB.swift".
--- Tests/TopTests/TopTests.swift
+++ Tests/TopTests/TopTests.swift
@@ -3,10 +3,14 @@ import XCTest
 
 private final class TopTests: XCTestCase {
     func testExample() {
+        // Fails if we comment out "import A" in AB.swift
         XCTAssertEqual(ab, ba)
-        print("*************", ab, "***************")
+        // Fails if we comment out "import A" in AB.swift
         XCTAssertEqual(idA, idB)
+        // Apparently always succeeds.
         XCTAssertEqual(ab, idA)
+        // Let's see which value we get when everything passes.
+        print("*************", ab, "***************")
     }
 
     static var allTests = [
